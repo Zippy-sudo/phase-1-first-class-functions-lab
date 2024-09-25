6091d269afee51531d5f51095407f9dc47a14acc
Nehemiah Madahana <nehemiahmadahana230@gmail.com>
2024-09-25 14:18:14 +0300

    First Trial

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const scuberDriverList = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = (a) => {
+    let newArray1 = a.slice(0,2);
+    return newArray1;
+}
+
+const returnLastTwoDrivers = (b) => {
+    let newArray2 = b.slice(b.length - 2);
+    return newArray2;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(c) {
+    return (fare) => fare * c;
+}
+
+const fareDoubler = fare => (createFareMultiplier(2))(fare);
+
+const fareTripler = fare => (createFareMultiplier(3))(fare);
+
+function selectDifferentDrivers(d, e) {
+    if (e === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(d);
+    } else return returnLastTwoDrivers(d);
+}
+
+console.log(selectDifferentDrivers(scuberDriverList, returnFirstTwoDrivers));
